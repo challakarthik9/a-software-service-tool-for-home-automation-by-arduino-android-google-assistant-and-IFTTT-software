ffe6795a310e32f6add47fff3198ac45b1eed402
challa karthik <36627188+challakarthik9@users.noreply.github.com>
2019-03-29 19:25:01 +0530

    Add files via upload

---
+++ PIR.ino/PIR.ino.ino
@@ -0,0 +1,24 @@
+int pir= 2;
+int led= 6;
+int val= 0;
+void setup() {
+  pinMode(pir,INPUT);
+  pinMode(led,OUTPUT); 
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  val= digitalRead(pir);
+  delayMicroseconds(100);
+  Serial.print(val);
+  if (val==1){
+    digitalWrite(led,HIGH);
+  }
+  if (val==0){
+    digitalWrite(led,LOW);
+  }
+  // put your main code here, to run repeatedly:
+
+}
